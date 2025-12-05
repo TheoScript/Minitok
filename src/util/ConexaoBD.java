78a5ce3210001bd01b067bced3447c99ee2f0d20
Rafael Oliveira <0001157509@senaimgaluno.com.br>
2025-12-04 23:04:06 -0300

    Conexão estabelecida

--- src/util/ConexaoBD.java
+++ src/util/ConexaoBD.java
@@ -1,4 +1,19 @@
 package util;
 
+import java.sql.*;
+
 public class ConexaoBD {
-}
+    private static final String URL = "jdbc:mysql://localhost:3306/minitok";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco!", e);
+        }
+    }
+
+    }
+
